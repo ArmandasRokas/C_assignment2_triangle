032db840a5d912f35572624f63b86fd3ca96f27a
Armandas <armandas.rokas@gmail.com>
2019-03-30 19:22:21 +0100

    made separate isRightTriangle function

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-
+int isRightTriangle(unsigned long a, unsigned long b, unsigned long c);
 int main() {
     unsigned long a, b, c;
     printf("Enter a, b, c:\n");
     scanf("%ld %ld %ld", &a, &b, &c);
-    if(a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b){
+    if(isRightTriangle(a,b,c)){
         printf("A right triangle exists");
     } else {
         printf("A right triangle NOT exists");
     }
     return 0;
+}
+
+int isRightTriangle(unsigned long a, unsigned long b, unsigned long c){
+    if(a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b){
+        return 1;
+    } else {
+        return 0;
+    }
 }
